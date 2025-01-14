3d8c352de286d52ed3118c053a7e370812603401
k4iLeb <40836142+k4iLeb@users.noreply.github.com>
2025-01-14 10:29:44 +0000

    Add Keyboard Support, Start work on CSS
    - WIP: work on checkPrio()

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function loadData() {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         data = await response.json();
-        console.log('Data loaded:', data);
+        // console.log('Data loaded:', data);
     } catch (error) {
         console.error('Error loading JSON:', error);
     }
@@ -19,34 +19,86 @@ const input = document.querySelector(".input")
 const searchBtn = document.querySelector(".btn")
 const container = document.querySelector(".container")
 const resultsDiv = document.querySelector(".results")
-// console.log(search);
-
+const list = document.querySelector(".list")
 
 // **** LISTENERS ****
 
 searchBtn.addEventListener("click", ()=> {
     const search = input.value.toUpperCase().split(' ')
-    const results = data.filter(item => Object.values(item).some(value => value.includes(search)))
-    console.log(search);
-    
-    console.log(results);
+    const results = data.filter(item => 
+        search.every(term => 
+            Object.values(item).some(value => value.toUpperCase().includes(term))
+        )
+      );
+
     input.focus()
     displayResults(results)
+})
+
+input.addEventListener("keydown", (e)=>{
+    if(e.key=="Enter"){
+        const search = input.value.toUpperCase().split(' ')
+    const results = data.filter(item => 
+        search.every(term => 
+            Object.values(item).some(value => value.toUpperCase().includes(term))
+        )
+      );
+
+    input.focus()
+    displayResults(results)
+    }
+    
 })
 
 // **** FUNCTIONS ****
 
 function displayResults(results){
-    // for(let item of results) {
-    //     let para = document.createElement('p')
-    //     para.textContent = `${results.Name}: ${results.Description} <<${results.Category}>>`
-    //     resultsDiv.appendChild(para)
-    //     console.log(item);
-        
-    // }
-    resultsDiv.innerHTML = results.map(row =>
-        `<div>${Object.entries(row).map(([key, value]) => `${value} `).join('')}</div>`
-    ).join('<hr>');
+    list.innerHTML = ""
+    for(let item of results) {
+        let listItem = document.createElement('li')
+        listItem.textContent = `${item.Name.trim()}: ${item.Description}`
+        list.appendChild(listItem)
+    }
+    checkPrio()
 }
+
+// **** check prio ****
+function checkPrio() {
+    let listItem = document.querySelectorAll('li')
+    // **** WIP ****
+    const highPrio = []
+    const midPrio = []
+    listItem.forEach((item)=> {
+        if(item.innerText.includes('OMERTA')) {
+            item.style.backgroundColor = "rgba(138, 195, 104, 0.6)"
+            console.log('hi');
+            
+        }
+    })
+    // console.log(listItem);
+    
+}
+
+// **** TEST ****
+
+// results = [
+//     {
+//         "Name": "CARAVELLA APPLE PIE TOBACCO",
+//         "Description": "ΑΜΕΡΙΚΑΝΙΚΟΣ ΚΑΠΝΟΣ, ΜΗΛΟΠΙΤΑ ( ΗΜΙΚΑΠΝΙΚΟ )",
+//         "Category": "ΗΜΙΚΑΠΝΙΚΟ"
+//     },
+//     {
+//         "Name": "RIPE VAPES APPLE TOBACCO",
+//         "Description": "ΜΗΛΟ , ΚΑΠΝΟΣ ( ΗΜΙΚΑΠΝΙΚΟ )",
+//         "Category": "ΗΜΙΚΑΠΝΙΚΟ"
+//     },
+//     {
+//         "Name": "OMERTA TERRA SAPPHIRE ",
+//         "Description": "ΚΛΑΣΙΚΟΣ ΚΑΠΝΟΣ , ΠΑΡΑΔΟΣΙΑΚΟ ΤΣΙΓΑΡΟ",
+//         "Category": "ΚΑΠΝΙΚΟ"
+//     }
+// ]
+
+// displayResults(results)
 
 window.onload = loadData;
