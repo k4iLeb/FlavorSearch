93e385f66d4e2c61111026302e27b396b006c8eb
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-14 18:59:19 +0200

    Fix CSS, Add prio explanation, Add effects
    TODO:
    
    - Fix data (remove category from description, fix name from source)
    - Sort according to priority

--- script.js
+++ script.js
@@ -3,101 +3,107 @@ let data = [];
 // Load data from the JSON file
 
 async function loadData() {
-    try {
-        const response = await fetch('data.json');
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        data = await response.json();
-        // console.log('Data loaded:', data);
-    } catch (error) {
-        console.error('Error loading JSON:', error);
+  try {
+    const response = await fetch("data.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    data = await response.json();
+    // console.log('Data loaded:', data);
+  } catch (error) {
+    console.error("Error loading JSON:", error);
+  }
 }
 
-const input = document.querySelector(".input")
-const searchBtn = document.querySelector(".btn")
-const container = document.querySelector(".container")
-const resultsDiv = document.querySelector(".results")
-const list = document.querySelector(".list")
+const input = document.querySelector(".input");
+const searchBtn = document.querySelector(".btn");
+const container = document.querySelector(".container");
+const resultsDiv = document.querySelector(".results");
+const list = document.querySelector(".list");
 
 // **** LISTENERS ****
 
-searchBtn.addEventListener("click", ()=> {
-    const search = input.value.toUpperCase().split(' ')
-    const results = data.filter(item => 
-        search.every(term => 
-            Object.values(item).some(value => value.toUpperCase().includes(term))
-        )
-      );
+searchBtn.addEventListener("click", () => {
+  search();
+});
 
-    input.focus()
-    displayResults(results)
-})
+input.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    search();
+    searchBtn.style.boxShadow = "inset 0px 0px 5px rgb(53, 52, 52)";
+  }
+});
 
-input.addEventListener("keydown", (e)=>{
-    if(e.key=="Enter"){
-        const search = input.value.toUpperCase().split(' ')
-    const results = data.filter(item => 
-        search.every(term => 
-            Object.values(item).some(value => value.toUpperCase().includes(term))
-        )
-      );
-
-    input.focus()
-    displayResults(results)
-    }
-    
-})
+input.addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    searchBtn.style.boxShadow = "none";
+  }
+});
 
 // **** FUNCTIONS ****
 
-function displayResults(results){
-    list.innerHTML = ""
-    for(let item of results) {
-        let listItem = document.createElement('li')
-        listItem.textContent = `${item.Name.trim()}: ${item.Description}`
-        list.appendChild(listItem)
-    }
-    checkPrio()
+function search() {
+  const search = input.value.toUpperCase().split(" ");
+  const results = data.filter((item) =>
+    search.every((term) =>
+      Object.values(item).some((value) => value.toUpperCase().includes(term))
+    )
+  );
+
+  input.focus();
+  displayResults(results);
+}
+
+function displayResults(results) {
+  list.innerHTML = "";
+  for (let item of results) {
+    let listItem = document.createElement("li");
+    listItem.textContent = `${item.Name.trim()}: ${item.Description}`;
+    list.appendChild(listItem);
+  }
+  checkPrio();
 }
 
 // **** check prio ****
 function checkPrio() {
-    let listItem = document.querySelectorAll('li')
-    // **** WIP ****
-    const highPrio = []
-    const midPrio = []
-    listItem.forEach((item)=> {
-        if(item.innerText.includes('OMERTA')) {
-            item.style.backgroundColor = "rgba(138, 195, 104, 0.6)"
-        }
-        item.style.backgroundColor = "rgb(212 217 219 / 80%)"
-    })
-    // console.log(listItem);
-    
+  let listItem = document.querySelectorAll("li");
+  // **** WIP ****
+  const highPrio = ["SWEET DREAMS", "CARAVELLA", "5SENSES"];
+  const midPrio = ["OMERTA", "THE DONS", "LA FAMIGLIA"];
+  listItem.forEach((item) => {
+    highPrio.forEach((flavor) => {
+      if (item.innerText.includes(flavor)) {
+        item.style.backgroundColor = "#A1DDED90";
+      }
+    });
+    midPrio.forEach((flavor) => {
+      if (item.innerText.includes(flavor)) {
+        item.style.backgroundColor = "rgba(138, 195, 104, 0.6)";
+      }
+    });
+  });
 }
 
 // **** TEST ****
 
 // results = [
-//     {
-//         "Name": "CARAVELLA APPLE PIE TOBACCO",
-//         "Description": "ΑΜΕΡΙΚΑΝΙΚΟΣ ΚΑΠΝΟΣ, ΜΗΛΟΠΙΤΑ ( ΗΜΙΚΑΠΝΙΚΟ )",
-//         "Category": "ΗΜΙΚΑΠΝΙΚΟ"
-//     },
-//     {
-//         "Name": "RIPE VAPES APPLE TOBACCO",
-//         "Description": "ΜΗΛΟ , ΚΑΠΝΟΣ ( ΗΜΙΚΑΠΝΙΚΟ )",
-//         "Category": "ΗΜΙΚΑΠΝΙΚΟ"
-//     },
-//     {
-//         "Name": "OMERTA TERRA SAPPHIRE ",
-//         "Description": "ΚΛΑΣΙΚΟΣ ΚΑΠΝΟΣ , ΠΑΡΑΔΟΣΙΑΚΟ ΤΣΙΓΑΡΟ",
-//         "Category": "ΚΑΠΝΙΚΟ"
-//     }
-// ]
+//   {
+//     Name: "CARAVELLA APPLE PIE TOBACCO",
+//     Description: "ΑΜΕΡΙΚΑΝΙΚΟΣ ΚΑΠΝΟΣ, ΜΗΛΟΠΙΤΑ",
+//     Category: "ΗΜΙΚΑΠΝΙΚΟ",
+//   },
+//   {
+//     Name: "RIPE VAPES APPLE TOBACCO",
+//     Description: "ΜΗΛΟ , ΚΑΠΝΟΣ",
+//     Category: "ΗΜΙΚΑΠΝΙΚΟ",
+//   },
+//   {
+//     Name: "OMERTA TERRA SAPPHIRE ",
+//     Description: "ΚΛΑΣΙΚΟΣ ΚΑΠΝΟΣ , ΠΑΡΑΔΟΣΙΑΚΟ ΤΣΙΓΑΡΟ",
+//     Category: "ΚΑΠΝΙΚΟ",
+//   },
+// ];
 
-// displayResults(results)
+// displayResults(results);
 
 window.onload = loadData;
